e5b4e2800469de2e93089585e33c79934edd4600
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-06-25 10:36:06 +1000

    format files and link css and js files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    $("ul.navbar > li").click(function (e) {
-        $("ul.navbar > li").removeClass("clicked");
-        $(this).addClass("clicked");
-    });
+  $("ul.navbar > li").click(function (e) {
+    $("ul.navbar > li").removeClass("clicked");
+    $(this).addClass("clicked");
+  });
 
-    $("ul.navbar > li").click(function (e) {
-        $("tab-count").removeClass("tab-count-clicked");
-        $(this).addClass("tab-count-clicked");
-    });
+  $("ul.navbar > li").click(function (e) {
+    $("tab-count").removeClass("tab-count-clicked");
+    $(this).addClass("tab-count-clicked");
+  });
 });
